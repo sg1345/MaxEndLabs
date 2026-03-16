eba53e91f3b274cfada3c9150abbbf44dc3175a8
sg1345 <dimitkostz@gmail.com>
2026-03-16 01:51:02 +0100

    Add Order scaffold: repo, service, controller
    Introduce initial Order feature scaffolding: add IOrderRepository and OrderRepository (inherits BaseRepository), add IOrderService interface, and add OrderController. Also add a Views\Order folder entry in the web project. These are placeholders with no methods/logic yet to set up the orders domain.

---
+++ MaxEndLabs.Data/Repository/OrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MaxEndLabs.Data.Repository.Contracts;
+
+namespace MaxEndLabs.Data.Repository
+{
+	public class OrderRepository : BaseRepository, IOrderRepository
+	{
+		public OrderRepository(MaxEndLabsDbContext dbContext) 
+			: base(dbContext)
+		{
+		}
+	}
+}
---
+++ MaxEndLabs.Web/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MaxEndLabs.Web.Controllers
+{
+	public class OrderController : BaseController
+	{
+		public IActionResult Index()
+		{
+			return Ok();
+		}
+	}
+}
