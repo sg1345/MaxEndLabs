8f16e6cf151ba17a04a0298e9d608ee727c8b32a
sg1345 <dimitkostz@gmail.com>
2026-04-20 23:20:02 +0200

    Add Migration
    Add EF Core migration (20260420211352_CreateNewsArticleTable) and update the DbContext model snapshot to create the NewsArticles table. Also adjust entity validation constants and appsettings.json to support the new entity and its constraints.

---
+++ MaxEndLabs.Data/Migrations/PostgresMigrations/20260420211352_CreateNewsArticleTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MaxEndLabs.Data.Migrations.PostgresMigrations
+{
+    /// <inheritdoc />
+    public partial class CreateNewsArticleTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NewsArticles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TeaserTitle = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    ContentTitle = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Summary = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    CoverImageUrl = table.Column<string>(type: "text", nullable: false),
+                    ArticleImageUrl = table.Column<string>(type: "text", nullable: true),
+                    PublishedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsPublished = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NewsArticles", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NewsArticles");
+        }
+    }
+}
