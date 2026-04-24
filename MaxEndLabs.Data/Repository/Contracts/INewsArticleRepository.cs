1b6ede19bbc78067041f8c708f22a30555977f61
sg1345 <dimitkostz@gmail.com>
2026-04-24 23:46:38 +0200

    Add NewsArticle repository and interface
    Introduce INewsArticleRepository contract and a concrete NewsArticleRepository. The interface declares async methods to get all articles, get an article by id, add an article, and save changes. The repository inherits BaseRepository and implements the data operations. SaveChangesAsync is provided via the base repository.

---
+++ MaxEndLabs.Data/Repository/Contracts/INewsArticleRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MaxEndLabs.Data.Models;
+
+namespace MaxEndLabs.Data.Repository.Contracts
+{
+    public interface INewsArticleRepository
+    {
+        Task<IEnumerable<NewsArticle>> GetAllNewsArticlesAsync();
+        Task<NewsArticle?> GetNewsArticleByIdAsync(Guid id);
+        Task AddNewsArticleAsync(NewsArticle newsArticle);
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ MaxEndLabs.Data/Repository/NewsArticleRepository.cs
@@ -0,0 +1,33 @@
+﻿using MaxEndLabs.Data.Models;
+using MaxEndLabs.Data.Repository.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaxEndLabs.Data.Repository
+{
+    public class NewsArticleRepository : BaseRepository, INewsArticleRepository
+    {
+        protected NewsArticleRepository(MaxEndLabsDbContext dbContext) 
+            : base(dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<NewsArticle>> GetAllNewsArticlesAsync()
+        {
+           return await DbContext.NewsArticles
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<NewsArticle?> GetNewsArticleByIdAsync(Guid id)
+        {
+            return await DbContext.NewsArticles
+                .AsNoTracking()
+                .SingleOrDefaultAsync(na => na.Id.Equals(id));
+        }
+
+        public async Task AddNewsArticleAsync(NewsArticle newsArticle)
+        {
+           await DbContext!.AddAsync(newsArticle);
+        }
+    }
+}
