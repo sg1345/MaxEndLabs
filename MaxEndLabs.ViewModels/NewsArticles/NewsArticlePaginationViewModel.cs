5308e6836652e632b5c07b363fb75560d824c8e7
sg1345 <dimitkostz@gmail.com>
2026-05-05 09:23:08 +0200

    Add NewsArticle view models
    Introduce two new view models under MaxEndLabs.ViewModels.NewsArticles to support article details and paginated listings.
    
    - NewsArticleDetailsViewModel: contains Id, ContentTitle, Content, and optional ArticleImageUrl for article detail views.
    - NewsArticlePaginationViewModel: contains CurrentPage, TotalPages, HasPreviousPage, HasNextPage, IEnumerable<NewsArticleSummaryViewModel> Articles, and optional SearchTerm for paginated listings.

---
+++ MaxEndLabs.ViewModels/NewsArticles/NewsArticlePaginationViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MaxEndLabs.ViewModels.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxEndLabs.ViewModels.NewsArticles
+{
+    public class NewsArticlePaginationViewModel
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public bool HasPreviousPage { get; set; }
+        public bool HasNextPage { get; set; }
+        public IEnumerable<NewsArticleSummaryViewModel> Articles { get; set; } = [];
+
+        public string? SearchTerm { get; set; }
+    }
+}
